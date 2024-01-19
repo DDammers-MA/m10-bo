021cdf2cffe48139733a01219df4ae909d195b62
Daniel <33257@ma-web.nl>
2024-01-19 17:58:37 +0100

    added slideShow

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -3,30 +3,47 @@ import { ActivatedRoute } from '@angular/router';
 import projects from '../../assets/json/info.json';
 import { CommonModule } from '@angular/common';
 
-
+interface ImageObject {
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.scss'],
   standalone: true,
-  imports: [CommonModule, ],
+  imports: [CommonModule],
 })
 export class ProjectDetailComponent implements OnInit {
   projecten = projects.projecten;
   project: any; // Adjust the type based on your project data structure
   imagePaths: string[] = [];
+  currentIndex = 0;
 
+ 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
-      console.log('Route Params:', params);
       const projectId = +params['id'];
       this.project = projects.projecten.find(project => project.id === projectId);
-      console.log('Project:', this.project);
- });
-  }
-}
 
+      this.imagePaths = this.project.image.map((imageObject: ImageObject) => imageObject.imageUrl);
+
+      // Reset currentIndex when a new project is loaded
+      this.currentIndex = 0;
+
+      console.log('Project:', this.project);
+      console.log('Image Paths:', this.imagePaths);
+    });
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.imagePaths.length) % this.imagePaths.length;
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.imagePaths.length;
+  }
+  }
 
---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,22 @@
+// slideshow.component.ts
+
+import { Component, Input } from '@angular/core';
+import projects from '../../assets/json/info.json';
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.css']
+})
+export class SlideshowComponent {
+  projecten = projects.projecten;
+  @Input() slides: any[] = [];
+  currentIndex = 0;
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+  }
+}
